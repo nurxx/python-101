02ab205d1a1cc17cfd38dda9823cbe894166bbab
nurxx <ilknurmustafa99@gmail.com>
2019-04-03 19:27:25 +0300

    modified read_input_file.py

--- week02/friday/read_input_file.py
+++ week02/friday/read_input_file.py
@@ -11,15 +11,12 @@ def read_input_file():
             dates+=[index]
     dates += [len(lines)]
     dates=[(dates[i]+1,dates[i+1]) for i in range(len(dates)-1)]
-    #print(dates)
-    #print(str_dates)
 
     for i in range(len(dates)):
         incomes=[]
         expenses=[]
         for index in range(dates[i][0],dates[i][1]):
             lines[index]=list(lines[index].split(', '))
-            #print(lines[index])
             info[str_dates[i]]={'income':incomes , 'expense' : expenses}
             if 'New Expense' in lines[index][2]:
                 expenses.append((float(lines[index][0]),lines[index][1]))
