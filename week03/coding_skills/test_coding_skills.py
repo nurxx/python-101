08f14933868b7c01b9023b87f69f0242e35b7f6f
nurxx <ilknurmustafa99@gmail.com>
2019-04-05 16:50:46 +0300

    added car_racing tests

---
+++ week03/coding_skills/test_coding_skills.py
@@ -0,0 +1,21 @@
+from coding_skills import *
+import unittest 
+
+class TestCodingSkills(unittest.TestCase):
+    def test_file_input(self):
+        filename = 'data.json'
+        with open(filename,'r') as f:
+            data = json.load(f)
+        self.assertTrue(type(data),type(dict()))
+
+    def test_conding_skills(self):
+        filename = 'data.json'
+        expected_data = {'C++': [(99, 'Cherna Ninja')], 'PHP': [(37, 'Rado Rado')],
+                         'Python': [(80, 'Ivo Ivo')], 'C#': [(70, 'Pavli Pavli')],
+                         'Haskell': [(70, 'Rado Rado')], 'Java': [(50, 'Rado Rado')],
+                         'JavaScript': [(62, 'Rosi Rosi')], 'Ruby': [(35, 'Rosi Rosi')],
+                         'CSS': [(99, 'Pavli Pavli')], 'C': [(99, 'Cherna Ninja')]}
+        self.assertTrue(coding_skills(filename),expected_data)
+
+if __name__ == '__main__':
+    unittest.main()
