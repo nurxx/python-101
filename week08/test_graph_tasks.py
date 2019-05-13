7853893b98b3b4257becccc8c7e2eb383d7bccbd
nurxx <ilknurmustafa99@gmail.com>
2019-05-13 10:03:33 +0300

    Add tests for deep_find.py & deep_apply.py

--- week08/test_graph_tasks.py
+++ week08/test_graph_tasks.py
@@ -1,25 +1,25 @@
 import unittest
 from deep_find_all import *
 from deep_update import *
+from deep_find import *
+from deep_apply import *
+
 
 class TestGraphTasks(unittest.TestCase):
     def setUp(self):
         self.data1 =  {
-                    'a':5,
-                    'b':4,
-                    'c':{
-                    'd':'4',
-                    11:11,
-                    'v':{
-                        'panda1':'hi'
+                        'a': 1,
+                        'b': {
+                            'c': 3,
+                            'd': [{'e': 5,
+                            (1,2):5
+                            }],
+                            'f': {
+                                'g': 6,
+                                11:11
+                            }
+                        }
                     }
-                    },
-                    'panda':[
-                    {
-                    'panda2':'hello'
-                    }
-                    ]
-                }
         self.data2 = {
             'a':5,
             'b':4,
@@ -62,6 +62,36 @@ class TestGraphTasks(unittest.TestCase):
                         ]
                     }
         self.assertEqual(deep_update(to_be_updated,'panda2','Hello I am HackPanda'),expected_output)
+
+    def test_when_deep_find_key_in_data_with_values_nested_dicts(self):
+        expected_output=self.data1['b']['f']['g']
+        self.assertTrue(deep_find_dfs(self.data1,'g'),expected_output)
+
+    def test_when_deep_find_key_in_data_with_values_list_of_dicts(self):
+        expected_output=self.data1['b']['d'][0]['e']
+        self.assertTrue(deep_find_dfs(self.data1,'e'),expected_output)
+
+
+    def test_when_deep_apply_func_to_all_keys_in_data(self):
+        def func(key):
+            return str(key)
+
+        expected_output={
+                        'a': 1,
+                        'b': {
+                            'c': 3,
+                            'd': [{'e': 5,
+                            '(1,2)':5
+                            }],
+                            'f': {
+                                'g': 6,
+                                '11':11
+                            }
+                        }
+                    }
+
+        deep_apply(func,self.data1)
+        self.assertTrue(self.data1,expected_output)
 
 if __name__ == '__main__':
     unittest.main()
